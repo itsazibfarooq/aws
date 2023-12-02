ae8d81ccc83656cb11269d2b5b39be63f41cfeb8
EHS Guest <ehsguest@ehslabdllfvfyx3axlywk.lan>
2023-12-02 17:21:16 -0500

    some api

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const power = document.getElementById('power');
 const res = document.getElementById('res');
 
 calculate.addEventListener('click', () => {
-    let number = base.value;
-    let n = power.value;
-    res.value = Math.pow(number, n);
+    // getResult(base.value, power.value);
+    res.value = Math.pow(base.value, power.value);
 });
+
+
+var getResult = (base, power) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var payload = JSON.stringify({"base":base,"power":power});
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: payload,
+        redirect: 'follow'
+    };
+
+    fetch("https://cndnheyapc.execute-api.us-east-2.amazonaws.com/dev", requestOptions)
+    .then(response => response.text())
+    .then(result =>  res.value = JSON.parse(JSON.parse(result).body).result)
+    .catch(error => console.log('error', error));
+}
